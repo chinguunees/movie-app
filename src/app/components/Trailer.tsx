7ebc2a93c7ec9973ac36653af335410e1b9acb76
Chinguunee <u.chingunn@gmail.com>
2026-02-23 10:47:54 +0800

    minor 2

--- src/app/components/Trailer.tsx
+++ src/app/components/Trailer.tsx
@@ -4,11 +4,11 @@ import { getMovieTrailer } from "@/lib/api";
 type MovieDetailsPageProps = {
   id: any;
 };
-export const Trailer = async ({ id }: MovieDetailsPageProps) => {
-  const { results: movies } = await getMovieTrailer(id);
-  return <div>PISDA WE
-    <div>
-        {movies.map((movie)=> )}
-    </div>
-  </div>;
-};
+// export const Trailer = async ({ id }: MovieDetailsPageProps) => {
+//   const { results: movies } = await getMovieTrailer(id);
+//   return <div>PISDA WE
+//     <div>
+//         {movies.map((movie)=> )}
+//     </div>
+//   </div>;
+// };
